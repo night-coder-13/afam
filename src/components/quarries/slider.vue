44352b17573ea4a0f241f65bf0f80f4e6c343e13
mehdi <nikanuser@gmail.com>
2022-09-03 22:31:22 +0430

    v11 slider2 quarries

--- src/components/quarries/slider.vue
+++ src/components/quarries/slider.vue
@@ -1,63 +1,68 @@
 
 <template>
 	 <swiper
+    ref="mySwiperRef"
     :spaceBetween="30"
     :centeredSlides="true"
     :autoplay="{
-      delay: 2500,
+      delay: 20000,
       disableOnInteraction: false,
     }"
     :pagination="{
       clickable: true,
     }"
-    :navigation="false"
+    :navigation="true"
+    :modules="modules"
     class="mySwiper cursor-pointer"
   >
   <swiper-slide  class="relative">
-    <div class="absolute top-60 left-20 rounded-lg shadow-lg text-white box--slider px-5 py-8">
-      <h3>Quarries</h3>
-      <p class="ml-3 mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dolor</p>
+    <div class="absolute top-52 left-20 rounded-lg shadow-lg text-white box--slider px-3 sm:px-5 py-2 sm:py-8 w-11/12 md:w-7/12">
+      <h3 >Quarries</h3>
+      <p class="ml-3 mt-1 h-16 md:h-32 overflow-hidden text-sm sm:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+      <button class="px-4 py-1 text-sm sm:text-base border rounded-md sm:border-gray-100 border-gray-500 mt-2 sm:mt-4 ml-10 hover:bg-gray-100 hover:text-black">Read more</button>
     </div>
     <v-lazy-image class="w-full h-full img-slider"
       :src-placeholder="require('../../assets/gif/loader-img.gif')"
       :src="require('../../assets/img/q1.jpg')" alt="slider"/> 
   </swiper-slide>
   <swiper-slide  class="relative">
-    <div class="absolute top-52 left-20 rounded-lg shadow-lg text-white box--slider px-5 py-8 w-7/12">
-      <h3>Quarries</h3>
-      <p class="ml-3 mt-1 h-32 overflow-hidden">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
-      <button class="px-4 py-1 border rounded-md border-gray-100 mt-4 ml-10 hover:bg-gray-100 hover:text-black">Read more</button>
+    <div class="absolute top-52 left-20 rounded-lg shadow-lg text-white box--slider px-3 sm:px-5 py-2 sm:py-8 w-11/12 md:w-7/12">
+      <h3 >Quarries</h3>
+      <p class="ml-3 mt-1 h-16 md:h-32 overflow-hidden text-sm sm:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+      <button class="px-4 py-1 text-sm sm:text-base border rounded-md sm:border-gray-100 border-gray-500 mt-2 sm:mt-4 ml-10 hover:bg-gray-100 hover:text-black">Read more</button>
     </div>
     <v-lazy-image class="w-full h-full img-slider"
       :src-placeholder="require('../../assets/gif/loader-img.gif')"
       :src="require('../../assets/img/q2.jpg')" alt="slider"/> 
   </swiper-slide>
-  <swiper-slide  class="relative">
-    <div class="absolute top-60 left-20 rounded-lg shadow-lg text-white box--slider px-5 py-8">
-      <h3>Quarries</h3>
-      <p class="ml-3 mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum dolor</p>
-    </div>
-    <v-lazy-image class="w-full h-full img-slider"
-      :src-placeholder="require('../../assets/gif/loader-img.gif')"
-      :src="require('../../assets/img/q3.jpg')" alt="slider"/> 
-  </swiper-slide>
+  
   
   </swiper>
 	
 </template>
 
-<script setup>
+<script >
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import VLazyImage from "v-lazy-image";
-
 
 import "../../../node_modules/swiper/swiper.min.css";
   // Import Swiper styles
   import 'swiper/css';
-  
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
+import { Autoplay, Pagination, Navigation } from "swiper";
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+    VLazyImage
+  },
+  setup() {
+    return {
+      modules: [Autoplay, Pagination, Navigation],
+    };
+  },
+};
 </script>
 <style scoped>
 .mySwiper{
@@ -67,6 +72,14 @@ import "swiper/css/navigation";
   backdrop-filter: blur(2px) brightness(0.9);
   border: 1px solid rgba(255, 255, 255, 0.656);
 }
-
+@media only screen and (max-width: 650px){
+  .box--slider{
+    color: #333;
+    top: unset;
+    background-color: #fff;
+    bottom: 30px;
+    left: 4%;
+  }
+}
 </style>
 
