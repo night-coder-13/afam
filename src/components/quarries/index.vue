18ccc56cfa29f15448915881d125f9e8251a7de1
mehdi <nikanuser@gmail.com>
2022-09-06 18:15:22 +0430

    v12 quarries end

--- src/components/quarries/index.vue
+++ src/components/quarries/index.vue
@@ -2,18 +2,22 @@
     <Header />
     <div class="w-full">
         <div id="header"></div>
-        <section>
+        <section class="w-11/12 md:w-9/12 lg:w-7/12 m-auto py-8">
             <h3>Our Travertine & Marble Quarries</h3>
-            <p class="ml-2 text-sm">Esfahan Travertine materials have been extracted from our quarries since 1993. From the same quarry basin the ancient Iranians extracted Travertine to build the most important Iranian cultural monuments. Today, our company continues to produce, extract, process and market Travertine & Marble and Made in Iran stone all over the world. Currently we operate throughout 24 hectares of land with three active quarries.</p>
-            <p class="ml-2 text-sm">Throughout this century of operations, our company has entered important partnership agreements with other leading quarries in the marble and granite field both in Iran and across the Globe. We offer on top of Roman Travertine materials additional natural stones seeking to be a reference point in qualitative stone supply leveraging on our Made in Iran brand.</p>
+            <p class="mt-2 ml-2 md:ml-4 text-gray-600 text-sm md:text-lg">Esfahan Travertine materials have been extracted from our quarries since 1993. From the same quarry basin the ancient Iranians extracted Travertine to build the most important Iranian cultural monuments. Today, our company continues to produce, extract, process and market Travertine & Marble and Made in Iran stone all over the world. Currently we operate throughout 24 hectares of land with three active quarries.</p>
+            <Slider class="my-4" />
+            <p class="mt-2 ml-2 md:ml-4 text-gray-600 text-sm md:text-lg">Our company pursues the entire production process from the extraction to the transformation of products. We extract over 30,000 m³ of travertine & marble products per year and through the employment of block cutters and gang-saws we reach a total output of 20,000 m² of finished products per month. The highly specialized team of workers allows to meet the design needs of the greatest architects, engineers and designers in the worlds market.</p>
+            
+            <img src="../../assets/img/map.jpg" class="w-full mt-20 mb-4" alt="">
+            <p class="mt-2 ml-2 md:ml-4 text-gray-600 text-sm md:text-lg">Throughout this century of operations, our company has entered important partnership agreements with other leading quarries in the marble and granite field both in Iran and across the Globe. We offer on top of Roman Travertine materials additional natural stones seeking to be a reference point in qualitative stone supply leveraging on our Made in Iran brand.</p>
         </section>
     </div>
-    <!-- <Footer /> -->
+    <Footer />
 </template>
 
 <script setup>
 import Header from '../home-sections/hed.vue'
-// import Footer from '../home-sections/footer.vue'
+import Footer from '../home-sections/footer.vue'
 import Slider from './slider.vue'
 import { onBeforeRouteLeave } from 'vue-router';
 onBeforeRouteLeave((to, from) =>{
@@ -24,5 +28,16 @@ onBeforeRouteLeave((to, from) =>{
 </script>
 
 <style scoped>
-
+#header{
+    background: url(../../assets/img/head-img-quarry.jpg);
+    height: 65vh;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 50% 80%;
+}
+@media only screen and (max-width: 991px){
+    #header{
+        padding-bottom: 40%;
+    }
+}
 </style>
--- src/components/quarries/slider.vue
+++ src/components/quarries/slider.vue
@@ -16,25 +16,17 @@
     class="mySwiper cursor-pointer"
   >
   <swiper-slide  class="relative ">
-    <div class="absolute top-52 left-20 rounded-lg shadow-lg text-white box--slider px-3 sm:px-5 py-2 sm:py-8 w-11/12 md:w-7/12">
-      <h3 >Quarries</h3>
-      <p class="ml-3 mt-1 h-16 md:h-32 overflow-hidden text-sm sm:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
-    </div>
+    
     <v-lazy-image class="w-full h-full img-slider"
     :src-placeholder="require('../../assets/gif/loader-img.gif')"
     :src="require('../../assets/img/q1.jpg')" alt="slider"/> 
   </swiper-slide>
   <swiper-slide  class="relative">
-    <div class="absolute top-52 left-20 rounded-lg shadow-lg text-white box--slider px-3 sm:px-5 py-2 sm:py-8 w-11/12 md:w-7/12">
-      <h3 >Quarries</h3>
-      <p class="ml-3 mt-1 h-16 md:h-32 overflow-hidden text-sm sm:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
-    </div>
     <v-lazy-image class="w-full h-full img-slider"
     :src-placeholder="require('../../assets/gif/loader-img.gif')"
     :src="require('../../assets/img/q2.jpg')" alt="slider"/> 
   </swiper-slide>
 </swiper>
-<div id="bullet" class="bg-red-400"></div>
 	
 </template>
 
@@ -65,17 +57,13 @@ export default {
     for (const p of pagination_bullet) {
       p.style.background="#33a1f5"
     }
-    const pagination=document.querySelector('.swiper-pagination')
-    document.getElementById('bullet').appendChild(pagination)
-    console.log(pagination)
-    
   }
 };
 </script>
 <style scoped>
-.mySwiper{
+/* .mySwiper{
   height: 90vh;
-}
+} */
 .box--slider{
   backdrop-filter: blur(2px) brightness(0.9);
   border: 1px solid rgba(255, 255, 255, 0.656);
@@ -88,9 +76,9 @@ export default {
     bottom: 30px;
     left: 4%;
   }
-  .mySwiper{
-  height: 65vh;
-}
+  /* .mySwiper{
+  height: 60vh;
+} */
 }
 </style>
 
